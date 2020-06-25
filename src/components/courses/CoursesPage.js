c574af67162cc512bf7c44c6280e73b66a42691b
Matthew Liu <mliu78@jh.edu>
2020-06-25 17:32:58 -0400

    Implemented Redux boilerplates and began implementing redux into app

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,28 +1,55 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import * as courseActions from '../../redux/actions/courseActions';
+import PropTypes from 'prop-types';
 
 class CoursesPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            course: {
-                title: ''
-            }
+
+    state = {
+        course: {
+            title: ''
         }
     }
 
-    handleChange(event) {
+
+    handleChange = event => {
         const course = { ...this.state.course, title: event.target.value }
         this.setState({ course });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.props.createCourse(this.state.course);
     }
 
     render() {
-        return (<form>
-            <h2>Courses</h2>
-            <h3>Add Course</h3>
-            <input type='text' onChange={this.handleChange} value={this.state.course.title} />
-            <input type='submit' value="Save" />
-        </form>)
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h2>Courses</h2>
+                <h3>Add Course</h3>
+                <input type='text' onChange={this.handleChange} value={this.state.course.title} />
+                <input type='submit' value="Save" />
+                {this.props.courses.map(course => (
+                    <div key={course.title}>{course.title}</div>
+                ))}
+            </form>
+        );
     }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+    courses: PropTypes.array.isRequired,
+    createCourse: PropTypes.func.isRequired,
+}
+
+function mapStateToProps(state) {
+    return { courses: state.courses }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        createCourse: course => dispatch(courseActions.createCourse(course)),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
